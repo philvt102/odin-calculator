00ce48256097c5c497c302cf5276868e3cf1cb45
philvt102 <philvt102@protonmail.com>
2026-03-10 12:55:57 -0400

    Complete first draft of logic

--- script.js
+++ script.js
@@ -3,6 +3,54 @@ lcd.textContent = `0000000000`;
 
 let keypad = document.querySelector(`#buttons`);
 
+let a, op, b;
+
+function input(input){  
+    if(input=="="){operate(a,op,b);}
+    if(Number.isFinite(+input)){
+        if(!a){
+            a = input;
+            lcd.textContent = a;
+            return;
+        }
+        else if(!op && a.length<10){
+            a+=input;
+            lcd.textContent = a;
+            return;
+        }
+        else if(op && !b){
+            b = input;
+            lcd.textContent = b;
+            return;
+        }
+        else if(op && b.length<10){
+            b+=input;
+            lcd.textContent = b;
+            return;
+        }
+    }
+    else{
+        op = input;
+        return;
+    }
+}
+
+function operate(a, op, b){
+    let A=Number(a);
+    let B = Number(b);
+    let output;
+    switch (op){
+        case "+": output = A+B;
+            break;
+        case "-": output = A-B;
+            break;
+        case "*": output = A*B;
+            break;
+        case "/": output = A/B;
+    }
+    lcd.textContent = output;
+}
+
 const keys = [7,8,9,`+`,4,5,6,`-`,1,2,3,`/`,`C`,0,`=`,`*`];
 keys.forEach((currentKey)=>{
     let newKey = document.createElement("div");
@@ -14,5 +62,6 @@ keys.forEach((currentKey)=>{
     newKey.style.display = "flex";
     newKey.style.justifyContent = "center";
     newKey.style.alignItems = "center";
+    newKey.addEventListener("click", ()=>{input(newKey.textContent)});
     keypad.appendChild(newKey);
 })
